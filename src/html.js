9ca5b65ad6f4e4bc5911964e126a997b62e69204
Kulwant Singh <kulwants@jcsoftwaresolution.com>
2019-10-15 20:25:10 +0530

    lading page

--- src/html.js
+++ src/html.js
@@ -8,7 +8,7 @@ export default function HTML(props) {
 
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <link rel="shortcut icon" href="img/fav.png" />
+        <link rel="shortcut icon" href="" />
 
         <meta name="author" content="CodePixar">
         </meta>
